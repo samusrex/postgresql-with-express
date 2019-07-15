09a137e88486208aa8312db9d176517d465c050b
samuel <samuel.sm@samsung.com>
2019-07-15 17:08:37 -0400

    add sequelize + migrations + postgreesql

---
+++ server/models/todo.js
@@ -0,0 +1,18 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Todo = sequelize.define('Todo', {
+    title: DataTypes.STRING,
+    allowNull: false,
+  }, {});
+ 
+  Todo.associate = function(models) {
+    
+    Todo.hasMany(models.TodoItem, {
+      foreignKey: 'todoId',
+      as: 'todoItems',
+    });
+    
+  
+  };
+  return Todo;
+};
