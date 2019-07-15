a76dc9ae59ce67ecf46508990da700fcc3f322db
samuel <samuel.sm@samsung.com>
2019-07-15 17:20:06 -0400

    message

--- app.js
+++ app.js
@@ -5,16 +5,21 @@ const bodyParser = require('body-parser')
 const app = express();
 
 app.use(logger('dev'));
-
 app.use(bodyParser.json());
-
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.get('*', (req,res)=> res.status(200).send(
+require('./server/routes')(app);
+
+/*app.get('*', (req,res)=> res.status(200).send(
     {
         message: 'Welcome to nothingness',
     }
-));
+)); */
+
+
+app.get('*', (req, res) => res.status(200).send({
+  message: 'Welcome to the beginning of nothingness.',
+}));
 
 
 module.exports = app;
